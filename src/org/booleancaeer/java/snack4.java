46af686d8ce3f13718ccfb6471e37513223decca
Andreaboldrin97 <boldrinandrea23@gmail.com>
2022-11-16 12:29:36 +0100

    snack 4 finish

--- src/org/booleancaeer/java/snack4.java
+++ src/org/booleancaeer/java/snack4.java
@@ -1,4 +1,7 @@
 package org.booleancaeer.java;
+
+import java.util.Random;
+
 //
 //Snack4 (Bonus)
 //Crea due array che hanno un numero di elementi diversi. Mostra a video dei nuovi elementi random 
@@ -10,6 +13,46 @@ package org.booleancaeer.java;
 public class snack4 {
 
 	public static void main(String[] args) {
+		// PRENDO LA CLASSE PER I NUMERI RANDOM
+		Random rand = new Random();
+		
+		
+		// CREO I DUE ARRAY DI LUNGHEZZA DIVERSA
+		
+			// ARRAY 1 PIU' CORTO
+		int[] array1 = new int[3];
+			// ARRAY 2 PIU' LUNGO
+		int[] array2 = new int[22];
+		
+		// ISTANZIO UN VARIABILE PER LA LUNGHEZZA
+		int lengthDiff = 0;
+
+		// IN UNA CONDIZIONE VEDO LA DIFFERENZA TRA GLI ARRAY
+		if (array1.length > array2.length) {
+
+			lengthDiff = array1.length - array2.length;
+			
+		} else if (array1.length < array2.length) {
+
+			lengthDiff = array2.length - array1.length;
+			
+		} else {
+
+			System.out.println("The arrays have the same length.");
+			return;
+		};
+
+		// STAMPO LA DIFFERENZA IN CONSOLE
+		System.out.println("DIFF : " + lengthDiff);
+		
+// UNA VOLTA TROVATA LA DIFFERENZA CICLO PER LA DIFFERENZA IN MODO DA OTTENERE NUMERI RANDOM
+		for(int i = 0; i < lengthDiff; i++) {
+			
+			// CREO UN NUMERO RANDOM E LO STAMPO DA 0-200
+			int randomNumber = rand.nextInt(201);
+			System.out.println(randomNumber);
+		}
+
 		
 	}
 	
